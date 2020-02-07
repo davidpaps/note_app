689f8b92f7a43cd8f163b0ecfc645f0d98dd8225
David Papamichael <davidpapamichael@gmail.com>
2020-02-07 17:41:15 +0000

    step 11 and 12 completed, event listener added for button click, and user input stored in event object

--- src/note-controller.js
+++ src/note-controller.js
@@ -32,15 +32,20 @@
   function showNoteOnCurrentPage() {
     showNote(getIDFromUrl(window.location));
   }
-  
   function getIDFromUrl(location) {
     return location.hash.split("#")[1];
   }
-  
   function showNote(ID) {
     document
       .getElementById("app")
       .innerHTML = noteController.noteList.showNotelist()[ID].showNote();
   }
+})();
 
+(function listenForSubmit() {
+  var element = document.getElementById("text")
+  element.addEventListener("submit", function(click) {
+    click.preventDefault();
+    console.log(click.srcElement.elements[0].value)
+  })
 })();
